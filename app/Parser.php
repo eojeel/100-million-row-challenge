b96665b505ab008a32f7b24168d4b70e284b800e
joe lee <13422738+eojeel@users.noreply.github.com>
2026-02-25 19:12:27 +0000

    1 million row challange

--- app/Parser.php
+++ app/Parser.php
@@ -1,13 +1,31 @@
 <?php
-
 namespace App;
-
-use Exception;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $file = fopen($inputPath, 'r');
+
+        $data = [];
+
+        while($line = fgets($file)) {
+
+            [$uri, $date] = explode(',', trim($line));
+
+            $path = parse_url($uri, PHP_URL_PATH);
+
+            $dateformat = date('Y-m-d', strtotime($date));
+            $data[$path][$dateformat] = ($data[$path][$dateformat] ?? 0) + 1;
+        }
+
+        foreach($data as &$uri)
+        {
+            ksort($uri);
+        }
+
+        fclose($file);
+
+        file_put_contents($outputPath, json_encode($data, JSON_PRETTY_PRINT));
     }
 }
